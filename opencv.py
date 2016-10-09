f547fc12e6c5d5a59b755af0612813f820388335
Varun Shenoy <varun.nshenoy@gmail.com>
2016-10-09 16:41:23 -0700

    update

--- opencv.py
+++ opencv.py
@@ -1,21 +1,45 @@
 import numpy as np
 import cv2
+import os
+
+images = []
+labels = []
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('prabh.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def addFaces(name, dir):
+    img = cv2.imread(dir)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        images.append(gray[y:y+h, x:x+w])
+        labels.append(name)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+for folders in os.walk('Pictures'):
+    for folder in os.listdir('Pictures'):
+        for file in os.walk('Pictures/' + folder):
+            arr = file[2]
+            for img in arr:
+                if img != ".DS_Store":
+                    print 'Pictures/' + folder + '/' + img
+                    addFaces(folder, 'Pictures/' + folder + '/' + img)
+
+recognizer = cv2.face.createLBPHFaceRecognizer()
+
+recognizer.train(images, np.array(labels))
+
+for file in os.walk('Pictures/Test'):
+    print file[2]
+    for name in arr:
+        if name != ".DS_Store":
+            img = cv2.imread('Pictures/Test/' + name)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                cv2.imshow('img',img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+                nbr_predicted, conf = recognizer.predict(gray[y: y + h, x: x + w])
+                print "{} as {} with {} confidence".format(name, nbr_predicted, conf)
